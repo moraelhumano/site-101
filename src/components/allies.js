acb5755adf0e14f2c7e520fac2ebfce564d26a54
unknown <somostech101@gmail.com>
2020-06-16 23:13:44 -0500

    se corrige logos de aliados responsive

--- src/components/allies.js
+++ src/components/allies.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 const Allies = () => (
-  <section className="container allies-container  container-presentation">
+  <section className="container allies-container  ">
     <h2 className="text-center">Trabajamos de la mano con las comunidades de talento y estos son algunos de nuestros aliados</h2>
     <div>
-      <a href="https://bit.ly/2Y9vj0C"><img src={require('../../static/assets/gdgunam.png')} className="logo img-presentation" /></a>
+      <a href="https://bit.ly/2Y9vj0C"><img src={require('../../static/assets/gdgunam.png')}  /></a>
       <a href="https://bit.ly/2zDjPci">
-      <img src={require('../../static/assets/9no.jpg')} className="logo img-presentation" />
+      <img src={require('../../static/assets/9no.jpg')}  />
       </a>
     </div>
   </section>

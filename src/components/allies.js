e4f087dc877a65b81e138bf3c596967cc948902b
somostech101 <moraelhumano@gmail.com>
2020-06-23 19:31:07 -0500

    se corrigen links de img aliados y target

--- src/components/allies.js
+++ src/components/allies.js
@@ -4,10 +4,8 @@ const Allies = () => (
   <section className="container allies-container  ">
     <h2 className="text-center">Trabajamos de la mano con el talento, estos son algunos de nuestros aliados</h2>
     <div>
-      <a href="https://bit.ly/2Y9vj0C"><img src={require('../../static/assets/gdgunam.png')}  /></a>
-      <a href="https://bit.ly/2zDjPci">
-      <img src={require('../../static/assets/9no.jpg')}  />
-      </a>
+      <a  href="https://bit.ly/2zDjPci" target="_blank"><img src={require('../../static/assets/gdgunam.png')}  /></a>
+      <a href="https://bit.ly/2Y9vj0C" target="_blank"><img src={require('../../static/assets/9no.jpg')}  /></a>
     </div>
   </section>
 )

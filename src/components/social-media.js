6de11f9f0a97bd75b86828d5bbedfcd7875adc8e
moraelhumano <moraelhumano@gmail.com>
2020-11-27 23:58:09 -0600

    se agrega nuevo componente de social media

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -1,26 +1,29 @@
 import React from "react"
 
 const SocialMedia = () => (
-  <section className="container presentation-container margin-top-lg container-presentation">
-      <iframe
-      width="560"
-      className="align-self"
-      height="315"
-      src="https://www.youtube.com/embed/AiGqigA9PCg"
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <section className="container presentation-container  margin-top-lg container-presentation">
     <iframe
       width="560"
       className="align-self"
       height="315"
-      src="https://www.youtube.com/embed/AiGqigA9PCg"
+      src="https://www.youtube.com/embed/yepmB0RghyU"
       frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
     ></iframe>
-  </section>
+  <div className="text-right margin-left-sm">
+    <h2 className="margin-top-zero">Aprender es más fácil en equipo.</h2>
+
+    <p>
+      Somos una comunidad de talento digital formada por programadores y
+      diseñadores . A los que además de la pasión por crear software de
+      impacto, nos une el gusto por <b>compartir</b> lo que sabemos, nuestro
+      objetivo es ayudar a <b>romper la brecha digital</b>, haciendo que los
+      usuarios dejen de ser solo usuarios y se conviertan también en
+      creadores.
+    </p>
+  </div>
+</section>
 )
 
 export default SocialMedia

af5884b2a630c1fc11fdfd6a7fa1109e0c327f97
moraelhumano <moraelhumano@gmail.com>
2020-12-08 23:51:00 -0600

    se soluciona problema de  componente responsivo de redes sociales

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
           Sitio y programas creados por Tech 101
           para el resto de la comunidad
         </p>
-      <div className="icons-rrss">
+      <div className="icons-rrss padding-responsive-contaner">
         <a href="https://www.facebook.com/somostech101" target="_blank"><img src={require("../../static/assets/facebook.png")} alt="facebook-logo" /></a>
         <a href="https://www.instagram.com/somostech101/" target="_blank"><img src={require("../../static/assets/instagram.png")} alt="facebook-logo" /></a>
         <a href="https://www.youtube.com/watch?v=yepmB0RghyU" target="_blank"><img src={require("../../static/assets/youtube.png")} alt="facebook-logo" /></a>
